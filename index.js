397bc8048b97797220b21a3de65e8b963b7dc497
Frank Schulz <Frank-Schulz@live.nmit.ac.nz>
2020-03-03 10:07:53 +1300

    Hello world

---
+++ index.js
@@ -0,0 +1,16 @@
+var number = 2;
+var number1 = 5;
+
+var min = number - number1;
+var plus = number + number1;
+
+var div = number / number1;
+var perc = number % number1;
+var mult = number * number1;
+
+console.log("min = " + min);
+console.log("Min =", min);
+console.log("Plus =", plus);
+console.log("Div =",div);
+console.log("Perc =",perc);
+console.log("Mult =",mult);
